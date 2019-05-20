ecc7f615fdafcd96ebf5048e67bd7ff8b1322c04
Vivek <vivek.xerago@gmail.com>
2019-05-20 17:54:19 +0530

    my java program

---
+++ src/main/java/JavaPractice/Array.java
@@ -0,0 +1,26 @@
+package JavaPractice;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int c[]={1,2,3,4,5,6,7,8,9};
+		
+		for(int i = 0;i<c.length;i++)
+		{
+			System.out.println(c[i]);
+			
+			if(c[i]==7)
+				
+			{
+				break;
+			}
+		}
+		
+		
+		
+		
+	}
+
+}
